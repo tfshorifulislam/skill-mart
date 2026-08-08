096836294a0a1c97dd7f3d3a7502168ef84bf6d6
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-09 01:32:18 +0600

    login page right side done

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,38 @@
+import Image from "next/image";
 
 const LoginPage = () => {
   return (
-    <div className="">
-      dsdlfjsdf
-    </div>
+    <section className="h-screen grid grid-cols-2">
+      <div className="bg-[#3525CD] p-16 ">
+        <span>
+          <Image
+            src={'/logo2.png'}
+            width={200}
+            height={200}
+            alt="logo"
+          />
+        </span>
+
+        <div className="relative flex flex-col justify-center items-center h-full">
+          <h1 className="relative font-bold text-5xl text-white">
+            Learn skills that <br />
+            move you forward.
+          </h1>
+
+          <Image
+          src={'/image.png'}
+          width={512}
+          height={382}
+          alt="avatar"
+          className="absolute -bottom-20"/>
+
+        </div>
+      </div>
+
+      <div>
+
+      </div>
+    </section>
   );
 };
 
